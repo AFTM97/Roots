d1c4cc11bfff911a4d9afa820c7476f71fc7f746
André Mendes <andrefilipemendes@gmail.com>
2019-08-13 16:35:28 +0100

    Olá princesa

--- app/src/main/java/com/example/roots/Class.kt
+++ app/src/main/java/com/example/roots/Class.kt
@@ -1,3 +1,23 @@
 package com.example.roots
 
-class Pessoa
+class  Person() {
+    var name: String = ""
+    var sname: String = ""
+    var birthyear: Int = 0
+    var deathyear: Int = 0
+
+    class Person(_name: String, _sname: String, _birthyear: Int, _deathyear: Int) {
+        val name =_name
+        val sname =_sname
+        val birthyear =_birthyear
+        val deathyear =_deathyear
+    }
+
+}
+
+
+class Node{
+    var brothers : List<Person> = mutableListOf(Person())
+    var mom : Person = Person()
+    var dad : Person = Person()
+}
